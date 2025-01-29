874b5080687f860e17810411578dcb91b291a12b
krishna <krishnakj3312@gmail.com>
2025-01-29 11:38:55 +0530

    custom api response and error handling

--- reactLearnings/tailwind/chai-backend/src/index.js
+++ reactLearnings/tailwind/chai-backend/src/index.js
@@ -9,7 +9,21 @@ dotenv.config({
 
 
 
-connectDB(); 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running  at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed", err);
+    
+})
+
+
+
+
 // import express from "express";
 // const app = exress();
 
