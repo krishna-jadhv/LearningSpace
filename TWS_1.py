0c7dc2c05531dec27cc73cf1ff1f9ce468eb5f0d
fourteenbyte <jadhavkk90@gmail.com>
2026-08-19 23:41:16 +0530

    python basics revision notes

---
+++ TWS_1.py
@@ -0,0 +1,5 @@
+num1= int(input("enter your favourite number: "))
+num2 = int(input("enter your second favourite number : "))
+
+print("Result: ",num1+num2)
+
---
+++ TWS_3_Loops.py
@@ -0,0 +1,11 @@
+# List -> Datastructure which can hold multiple values of multiple types
+
+list_of_cloud = ["aws","azure","gcp","digital","alibab","amazon","google","xxxx"]
+
+cloud= "gcp"
+
+list_of_cloud.append("TCS")
+
+list_of_cloud.insert(2,"krishna")
+for cloud in list_of_cloud:
+    print(cloud)
