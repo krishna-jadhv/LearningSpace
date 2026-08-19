7eb3d30a1fc009aef9220451dcb789b567e4676d
fourteenbyte <jadhavkk90@gmail.com>
2026-08-19 23:41:16 +0530

    python basics revision notes

---
+++ TWS_2_Conditionals.py
@@ -0,0 +1,8 @@
+day_of_week= input("Enter the day of week : ").lower()
+print(day_of_week)
+
+if day_of_week=="sunday"or day_of_week=="saturday":
+    print("I will edit video and create content only")
+else:
+    print("I will study technical and devops")
+
